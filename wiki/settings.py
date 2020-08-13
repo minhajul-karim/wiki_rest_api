f9eeb5742fbb37614cb36258e9334282fce38c07
minhajul-karim <minhajul.kaarim@gmail.com>
2020-08-13 12:39:12 +0600

    Add Heroku url in Allowed Host

--- wiki/settings.py
+++ wiki/settings.py
@@ -35,7 +35,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',
+                 '0.0.0.0', 'wiki-rest-api.herokuapp.com']
 
 
 # Application definition
